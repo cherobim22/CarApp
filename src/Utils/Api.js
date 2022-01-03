ad4bfe6b15c21aef7144778d767fde5ea409fa5c
Lucas cherobim <lucascherobim@hotmail.com>
2022-01-03 20:48:29 -0300

    update base api

--- src/Utils/Api.js
+++ src/Utils/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://ec2-3-19-206-15.us-east-2.compute.amazonaws.com',
+    baseURL: 'https://cars-dev-br.herokuapp.com',
     timeout: 5000
 })
 
